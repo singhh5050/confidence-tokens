ef94f80dc8f27788583f29a899e1eb2e8b3f61d4
Harsh Singh <singhh5050@gmail.com>
2026-01-08 18:46:07 -0800

    Use ALL_LAYERNORM_LAYERS from transformers.pytorch_utils

--- src/training.py
+++ src/training.py
@@ -20,7 +20,8 @@ from transformers import (
     PreTrainedModel,
     PreTrainedTokenizer,
 )
-from transformers.trainer_pt_utils import get_parameter_names, ALL_LAYERNORM_LAYERS
+from transformers.trainer_pt_utils import get_parameter_names
+from transformers.pytorch_utils import ALL_LAYERNORM_LAYERS
 from datasets import Dataset
 
 
